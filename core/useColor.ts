058b5164ae0e89e9130d0ae862727c5417f1ce28
robertolima-dev <contato@robertolima.eti.br>
2023-04-04 13:24:02 -0300

    cor verde

--- core/useColor.ts
+++ core/useColor.ts
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 export default function useColor() {
 
-    const [color, setColor] = useState('blue')
+    const [color, setColor] = useState('green')
 
     return {
         color,
